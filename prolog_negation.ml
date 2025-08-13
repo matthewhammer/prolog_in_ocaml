bfb5be56e00dfca7fa65930baef46a25ff6360fa
Matthew Hammer <matthewhammer@gmail.com>
2025-08-13 12:46:52 -0600

    second try.

--- prolog_negation.ml
+++ prolog_negation.ml
@@ -1,9 +1,9 @@
 (* mini_prolog.ml
-   A tiny, purely functional Prolog with negation-as-failure (\+/1) and cut (!).
-   One-file, self-contained demo.
+   A tiny, purely functional Prolog core with negation-as-failure (\+/1) and cut (!/0).
+   Includes minimal builtins (= /2, =</2, true/0, fail/0), freshening, and tests.
 
-   Compile: ocamlc -o mini_prolog mini_prolog.ml
-   Run:     ./mini_prolog
+   Build: ocamlc -o mini_prolog mini_prolog.ml
+   Run:   ./mini_prolog
 *)
 
 (* ---------- Syntax ---------- *)
@@ -14,19 +14,18 @@ type term =
   | Comp of string * term list
 
 type goal =
-  | Call of term               (* predicate call *)
-  | Cut                        (* ! *)
-  | Neg of goal                (* \+ G *)
+  | Call of term            (* predicate call *)
+  | Cut                     (* ! *)
+  | Neg of goal             (* \+ G *)
 
 type clause = { head : term; body : goal list }
-
 type program = clause list
 
 (* ---------- Pretty printing ---------- *)
 
 let rec pp_term = function
-  | Var x      -> x
-  | Atom a     -> a
+  | Var x       -> x
+  | Atom a      -> a
   | Comp (f,[]) -> f
   | Comp (f,ts) ->
       let args = String.concat ", " (List.map pp_term ts) in
@@ -37,14 +36,8 @@ and pp_goal = function
   | Cut    -> "!"
   | Neg g  -> "\\+(" ^ pp_goal g ^ ")"
 
-and pp_goal_list gs =
-  match gs with
-  | [] -> "true"
-  | _  -> String.concat ", " (List.map pp_goal gs)
-
 (* ---------- Substitutions & unification ---------- *)
 
-(* Substitution: maps variable names to terms *)
 type subst = (string * term) list
 
 let rec lookup x (s:subst) =
@@ -62,7 +55,6 @@ let rec apply s = function
   | Comp (f,ts) -> Comp (f, List.map (apply s) ts)
 
 let compose s1 s2 =
-  (* apply s1 to range of s2, then append s1 bindings (shadowed by s2) *)
   let s2' = List.map (fun (x,t) -> (x, apply s1 t)) s2 in
   s2' @ s1
 
@@ -104,7 +96,6 @@ let fresh_counter =
   fun () -> incr c; !c
 
 let freshen_clause (cl:clause) : clause =
-  (* Map each Var "X" in clause to a unique Var "X#n" *)
   let tbl : (string, string) Hashtbl.t = Hashtbl.create 16 in
   let rename_var x =
     match Hashtbl.find_opt tbl x with
@@ -125,203 +116,226 @@ let freshen_clause (cl:clause) : clause =
   in
   { head = fterm cl.head; body = List.map fgoal cl.body }
 
-(* ---------- Proof search with cut & negation ---------- *)
+(* ---------- Cut signalling ---------- *)
 
-(* A result carries a substitution and an optional "cut barrier":
-   Some n  = a cut (!) was executed that commits up to barrier level n.
-   None    = no cut pressure to propagate.
+type cut_sig = NoCut | CutTo of int
+
+let cut_frame_counter =
+  let c = ref 0 in
+  fun () -> incr c; !c
+
+(* ---------- Builtins ---------- *)
+
+let is_int_string s =
+  try ignore (int_of_string s); true with _ -> false
+
+let eval_int s t =
+  match apply s t with
+  | Atom a when is_int_string a -> Some (int_of_string a)
+  | _ -> None
+
+(* ---------- Solver ---------- *)
+
+(* Core solver: DFS with proper cut semantics.
+   - 'frame' is the current predicate-call frame id (0 at top-level).
 *)
-type cut_barrier = int option
-type answer = subst * cut_barrier
+let rec solve (prog:program) (goals:goal list) (s:subst) (frame:int) : (subst * cut_sig) list =
+  match goals with
+  | [] -> [ (s, NoCut) ]
+  | g :: gs ->
+      let sols_first = solve_one prog g s frame in
+      (* Conjunctive sequencing with cut-awareness:
+         If any combined answer carries CutTo c where c <= frame,
+         we STOP exploring further alternatives for this goal at this frame. *)
+      let rec loop sols acc =
+        match sols with
+        | [] -> List.rev acc
+        | (s1, c1) :: more ->
+            let rest = solve prog gs s1 frame in
+            (* Propagate earlier cut if rest has none *)
+            let combined =
+              List.map (fun (s2, c2) ->
+                match c2 with
+                | NoCut -> (s2, c1)
+                | _ -> (s2, c2)
+              ) rest
+            in
+            let acc' = List.rev_append combined acc in
+            let committed =
+              List.exists (function
+                | (_, CutTo c) when c <= frame -> true
+                | _ -> false
+              ) combined
+            in
+            if committed then List.rev acc'
+            else loop more acc'
+      in
+      loop sols_first []
+and solve_one (prog:program) (g:goal) (s:subst) (frame:int) : (subst * cut_sig) list =
+  match g with
+  | Cut ->
+      (* Cut commits to the current frame; choices created before this point at [frame] are pruned. *)
+      [ (s, CutTo frame) ]
 
-(* Prover signature:
-   prove goals subst level barrier program
-   - level: current search depth (for clarity in debugging)
-   - barrier: the nearest predicate-call barrier; cut (!) commits up to this barrier
-*)
-let rec prove (gs:goal list) (s:subst) (level:int) (barrier:int) (prog:program) : answer list =
-  match gs with
-  | [] -> [ (s, None) ]
-  | g :: rest ->
-      begin match g with
-      | Cut ->
-          (* Cut: succeed immediately, but signal commitment to current barrier. *)
-          [ (s, Some barrier) ]
+  | Neg inner ->
+      (* Negation-as-failure: run inner in an isolated frame; succeed iff no solutions. *)
+      let _inner_frame = cut_frame_counter () in
+      let sols = solve prog [inner] s _inner_frame in
+      if sols = [] then [ (s, NoCut) ] else []
 
-      | Neg inner ->
-          (* Negation-as-failure: run a private proof of [inner].
-             If it has any solutions, negation fails; else it succeeds
-             with current substitution and no bindings exported.
-          *)
-          let sols = prove [inner] s (level+1) (level+1) prog in
-          if sols = [] then [ (s, None) ] else []
+  | Call (Atom "true") -> [ (s, NoCut) ]
+  | Call (Atom "fail") -> []
 
-      | Call (Atom "true") ->
-          prove rest s level barrier prog
-
-      | Call (Atom "fail") ->
-          []
-
-      | Call (Comp (pred, args)) ->
-          (* Try all matching clauses of arity n *)
-          let arity = List.length args in
-          let clauses =
-            List.filter (fun {head; _} ->
-              match head with
-              | Comp (p, hs) -> p = pred && List.length hs = arity
-              | Atom p       -> p = pred && 0 = arity
-              | _ -> false
-            ) prog
-          in
-          prove_over_clauses clauses args rest s level barrier prog
-
-      | Call (Atom p) ->
-          (* 0-arity predicate *)
-          let clauses =
-            List.filter (fun {head; _} ->
-              match head with
-              | Atom q -> q = p
-              | Comp (q,hs) -> q = p && List.length hs = 0
-              | _ -> false
-            ) prog
-          in
-          prove_over_clauses clauses [] rest s level barrier prog
-
-      | Call (Var _) ->
-          failwith "Callable variable not supported in this mini-interpreter"
-
+  | Call (Comp ("=", [t1; t2])) ->
+      begin match unify t1 t2 s with
+      | None -> []
+      | Some s' -> [ (s', NoCut) ]
       end
 
-and prove_over_clauses (clauses:clause list) (args:term list) (rest:goal list)
-                       (s:subst) (level:int) (barrier:int) (prog:program)
-  : answer list =
+  | Call (Comp ("=<", [t1; t2])) ->
+      begin match eval_int s t1, eval_int s t2 with
+      | Some i, Some j -> if i <= j then [ (s, NoCut) ] else []
+      | _ -> []  (* require ground ints for this demo *)
+      end
+
+  | Call (Comp (pred, args)) ->
+      (* User predicate *)
+      let arity = List.length args in
+      let matching =
+        List.filter (fun {head; _} ->
+          match head with
+          | Atom p       -> (p = pred) && (arity = 0)
+          | Comp (p, hs) -> (p = pred) && (List.length hs = arity)
+          | _ -> false
+        ) prog
+      in
+      solve_call prog pred args matching s frame
+
+  | Call (Atom p) ->
+      (* 0-arity form *)
+      let matching =
+        List.filter (fun {head; _} ->
+          match head with
+          | Atom q       -> q = p
+          | Comp (q, hs) -> (q = p) && (List.length hs = 0)
+          | _ -> false
+        ) prog
+      in
+      solve_call prog p [] matching s frame
+
+  | Call (Var _) ->
+      failwith "Callable variables are not supported in this mini-interpreter"
+and solve_call (prog:program) (pred:string) (args:term list)
+               (clauses:clause list) (s:subst) (parent_frame:int)
+  : (subst * cut_sig) list =
+  (* Evaluate a predicate call by trying matching clauses.
+     Each clause body runs in its own new frame.
+     If a body produces CutTo that frame, we stop trying further clauses.
+     We **swallow** the cut before returning upward (cut is local to this call). *)
   let rec loop cls acc =
     match cls with
     | [] -> List.rev acc
     | cl :: more ->
         let cl = freshen_clause cl in
-        (* Try to unify head with the call *)
-        begin match cl.head with
-        | Atom _ | Var _ -> (* normalized away in freshen; ignore *)
-            loop more acc
-        | Comp (p, hs) ->
-            begin match unify (Comp (p, hs)) (Comp (p, args)) s with
-            | None ->
-                loop more acc
-            | Some s' ->
-                (* For the body of this clause, establish a barrier at THIS predicate call level. *)
-                let body_goals = cl.body @ rest in
-                let results = prove body_goals s' (level+1) level prog in
-                (* If any result indicates a cut to <= level, stop exploring other clauses. *)
-                let acc' = List.rev_append results acc in
-                let cut_here =
-                  List.exists (function
-                    | (_, Some b) when b <= level -> true
-                    | _ -> false
-                  ) results
-                in
-                if cut_here then List.rev acc'
-                else loop more acc'
-            end
+        begin
+          let head_term =
+            match cl.head with
+            | Atom p       -> Comp (p, [])
+            | Comp (p, hs) -> Comp (p, hs)
+            | Var _        -> failwith "Invalid clause head (Var)"
+          in
+          match unify head_term (Comp (pred, args)) s with
+          | None ->
+              loop more acc
+          | Some s' ->
+              let frame' = cut_frame_counter () in
+              let sols = solve prog cl.body s' frame' in
+              let committed_here =
+                List.exists (function
+                  | (_, CutTo c) when c = frame' -> true
+                  | _ -> false
+                ) sols
+              in
+              (* Swallow local cut signals; they shouldn't escape this call. *)
+              let sols' = List.map (fun (s_ans, _) -> (s_ans, NoCut)) sols in
+              let acc' = List.rev_append sols' acc in
+              if committed_here then List.rev acc'
+              else loop more acc'
         end
   in
   loop clauses []
 
-(* ---------- A tiny test program ---------- *)
+(* ---------- Reification & printing ---------- *)
 
-(* max/3 with cut: chooses the first true branch and commits *)
-let cl_max1 =
-  { head = Comp("max", [Var "X"; Var "Y"; Var "X"]);
+let reify_term s t = apply s t
+
+let show_answers varnames (answers:(subst * cut_sig) list) =
+  match answers with
+  | [] -> print_endline "no."
+  | _ ->
+      List.iter (fun (s, _c) ->
+        let items =
+          List.map (fun v ->
+            let tv = reify_term s (Var v) in
+            v ^ " = " ^ pp_term tv
+          ) varnames
+        in
+        print_endline ("{ " ^ String.concat "; " items ^ " }")
+      ) answers
+
+let run_query ~prog ~vars goals =
+  let ans = solve prog goals [] 0 in
+  show_answers vars ans
+
+(* ---------- Test program ---------- *)
+
+(* max/3 with cut: if X =< Y then pick Y and commit; else pick X *)
+let cl_max_if =
+  { head = Comp("max", [Var "X"; Var "Y"; Var "Y"]);
     body = [ Call (Comp("=<", [Var "X"; Var "Y"])); Cut ] }
 
-let cl_max2 =
-  { head = Comp("max", [Var "X"; Var "Y"; Var "Y"]);
+let cl_max_else =
+  { head = Comp("max", [Var "X"; Var "Y"; Var "X"]);
     body = [] }
 
-(* A “<=” relation for small integers (Peano-free, hand-coded facts). *)
-let le_facts =
-  let ints = [0;1;2;3;4;5] in
-  let mk a b =
-    { head = Comp("=<", [Atom (string_of_int a); Atom (string_of_int b)]); body = [] }
-  in
-  let pairs =
-    List.concat (List.map (fun a -> List.filter (fun b -> a <= b) ints |> List.map (fun b -> mk a b)) ints)
-  in
-  pairs
-
-(* parent facts & a not_parent/2 rule using negation-as-failure *)
+(* parents *)
 let parent_facts = [
   { head = Comp("parent", [Atom "alice"; Atom "bob"]); body = [] };
   { head = Comp("parent", [Atom "carol"; Atom "dave"]); body = [] };
 ]
 
+(* not_parent(X,Y) :- \+ parent(X,Y). *)
 let not_parent_rule =
   { head = Comp("not_parent", [Var "X"; Var "Y"]);
     body = [ Neg (Call (Comp("parent", [Var "X"; Var "Y"]))) ] }
 
-(* A fact we “add later” conceptually to show non-monotonicity idea; we will
-   run two queries: before and after adding this to the program list. *)
+(* Fact we add later to demonstrate non-monotonicity *)
 let new_parent_fact =
   { head = Comp("parent", [Atom "alice"; Atom "charlie"]); body = [] }
 
 let prog_base : program =
-  [ cl_max1; cl_max2; not_parent_rule ] @ le_facts @ parent_facts
+  [ cl_max_if; cl_max_else; not_parent_rule ] @ parent_facts
 
 let prog_plus : program =
   new_parent_fact :: prog_base
 
-(* ---------- Query driver ---------- *)
-
-let rec reify_term (s:subst) (t:term) = apply s t
-
-let show_answers varnames (answers:answer list) =
-  let show_one (s, _cut) =
-    let items =
-      List.map (fun v ->
-        let tv = reify_term s (Var v) in
-        v ^ " = " ^ pp_term tv
-      ) varnames
-    in
-    print_endline ("{ " ^ String.concat "; " items ^ " }")
-  in
-  match answers with
-  | [] -> print_endline "no."
-  | _  -> List.iter show_one answers
-
-(* A helper to make running a single query easier *)
-let run_query ~prog ~vars (goals:goal list) =
-  let ans = prove goals [] 0 0 prog in
-  show_answers vars ans
-
 (* ---------- Demos ---------- *)
 
 let () =
-  print_endline "== Demo 1: max/3 with cut ==";
+  print_endline "== Demo 1: max/3 with cut (one answer) ==";
   let q1 = [ Call (Comp("max", [Atom "2"; Atom "5"; Var "M"])) ] in
   run_query ~prog:prog_base ~vars:["M"] q1;
-  (* There is exactly one answer due to cut: M = 5 *)
 
-  print_endline "\n== Demo 2: negation-as-failure (before adding a fact) ==";
-  (* Ask for Y such that alice is NOT parent of Y *)
-  let q2 = [ Call (Comp("not_parent", [Atom "alice"; Var "Y"])) ] in
-  run_query ~prog:prog_base ~vars:["Y"] q2;
-  (* Expect Y can be any term not proven as alice's child; with only parent(alice,bob),
-     \+ parent(alice,Y) fails for Y=bob but succeeds for other values.
-     Our tiny interpreter doesn’t enumerate the universe; this demo just shows
-     that Y unifies with what remains in other goals—here there are none, so it
-     yields the current (empty) substitution: effectively success without bindings. *)
+  print_endline "\n== Demo 2a: negation-as-failure BEFORE adding fact ==";
+  (* Constrain Y first, then negate: succeeds because parent(alice,charlie) is absent. *)
+  let q2a = [ Call (Comp("=", [Var "Y"; Atom "charlie"]));
+              Call (Comp("not_parent", [Atom "alice"; Var "Y"])) ] in
+  run_query ~prog:prog_base ~vars:["Y"] q2a;
 
-  print_endline "\n== Demo 3: non-monotonicity of \\+ by adding a fact ==";
-  (* Same query, but “after” adding parent(alice,charlie). This can flip success/failure
-     in contexts where a particular Y gets bound earlier. We demonstrate by constraining Y. *)
-  let q3 =
-    [ Call (Comp("=", [Var "Y"; Atom "charlie"]));
-      Call (Comp("not_parent", [Atom "alice"; Var "Y"])) ]
-  in
-  print_endline "-- Before adding fact parent(alice,charlie) --";
-  run_query ~prog:prog_base ~vars:["Y"] q3;   (* succeeds: \+ parent(alice,charlie) held before *)
-
-  print_endline "-- After adding fact parent(alice,charlie) --";
-  run_query ~prog:prog_plus ~vars:["Y"] q3;   (* now fails: negation flips due to new fact *)
+  print_endline "\n== Demo 2b: negation-as-failure AFTER adding fact (non-monotonic flip) ==";
+  let q2b = [ Call (Comp("=", [Var "Y"; Atom "charlie"]));
+              Call (Comp("not_parent", [Atom "alice"; Var "Y"])) ] in
+  run_query ~prog:prog_plus ~vars:["Y"] q2b;
 
   print_endline "\nDone."
+
